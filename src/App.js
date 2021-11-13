b0f6038eb763d58ae5142e3ddd0965d4249837d2
stan000 <stanmpakatig@gmail.com>
2021-11-13 21:23:10 +0200

    Set up and tested API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+import { API_URL } from "./utils/urls";
+import CurrencyComponent from "./CurrencyComponent";
 
 function App() {
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((currencyData) => console.log(currencyData));
+  }, []);
+
   return (
-    <h1>Hello Shumba</h1>
+    <>
+      <h1>Shumba Money Exchange rate calculator</h1>
+      <div>
+        <CurrencyComponent />
+        <div>Icon</div>
+        <CurrencyComponent />
+      </div>
+    </>
   );
 }
 
---
+++ src/utils/urls.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  "http://api.exchangeratesapi.io/v1/latest?access_key=9f3ee5958f05a622536e80ab84ebfc3f&format=1";
